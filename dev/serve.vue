8ab2a77c0e192b3132916a076df69e0322f058f5
Dane Iracleous <daneiracleous@gmail.com>
2020-09-03 18:58:51 -0700

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,70 @@
+<script>
+import Vue from 'vue';
+import VueEnlargeableImage from '@/vue-enlargeable-image.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueEnlargeableImage
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <div class="test-container">
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+      <div>
+        <vue-enlargeable-image src="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=640" src_large="https://unsplash.com/photos/e3PoGwCFQnM/download?force=true&w=2400" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.test-container {
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-wrap:wrap;
+}
+.test-container > div {
+  width:200px;
+  margin:30px;
+}
+</style>
---
+++ src/vue-enlargeable-image.vue
@@ -0,0 +1,128 @@
+<script>
+export default {
+  name: 'VueEnlargeableImage',
+  props: {
+    src: {
+      type: String
+    },
+    src_large: {
+      type: String
+    },
+  },
+  data() {
+    return {
+      enlarging: this.enlarging,
+      enlarged: this.enlarged,
+      styles: this.styles
+    };
+  },
+  methods: {
+    init() {
+      var self = this;
+      self.updating = false;
+      self.enlarging = false;
+      self.enlarged = false;
+      self.styles = {};
+    },
+    enlarge(event) {
+      var self = this;
+      if(!self.updating) {
+        self.updating = true;
+        self.$nextTick(function() {
+          var img = self.$refs.img;
+          var rect = img.getBoundingClientRect();
+          self.styles = {
+            position: "absolute",
+            left: Math.round(rect.left)+"px",
+            top: Math.round(rect.top)+"px",
+            width: Math.floor(rect.right - rect.left)+"px",
+            height: Math.floor(rect.bottom - rect.top)+"px",
+            backgroundImage: "url("+self.$props.src+")",
+          };
+          self.enlarging = true;
+          setTimeout(function() {
+            self.styles = {
+              backgroundImage: "url("+self.$props.src+")",
+            };
+            setTimeout(function() {
+              self.enlarged = true;
+              self.updating = false;
+            }, 1400);
+          }, 0);
+        });
+      }
+    },
+    reset(event) {
+      var self = this;
+      if(!self.updating) {
+        self.updating = true;
+        var img = self.$refs.img;
+        var rect = img.getBoundingClientRect();
+        self.enlarging = true;
+        setTimeout(function() {
+          self.enlarged = false;
+          self.styles = {
+            backgroundImage: "url("+self.$props.src+")",
+            position: "fixed",
+            left: Math.round(rect.left)+"px",
+            top: Math.round(rect.top)+"px",
+            width: Math.floor(rect.right - rect.left)+"px",
+            height: Math.floor(rect.bottom - rect.top)+"px",
+          };
+          setTimeout(function() {
+            self.enlarged = false;
+            self.enlarging = false;
+            self.updating = false;
+          }, 1400);
+        }, 0);
+      }
+    }
+  },
+  mounted: function() {
+    var self = this;
+    self.init();
+  }
+};
+</script>
+
+<template>
+  <div class="vue-enlargeable-image">
+    <img :src="this.$props.src" @click="enlarge($event)" ref="img" />
+    <div v-if="enlarging" @click="reset($event)" class="vue-enlargeable-image-full" v-bind:style="styles" v-bind:class="{ enlarging: enlarging, enlarged: enlarged }">
+      <img v-if="!enlarged" :src="this.$props.src" />
+      <img v-if="enlarged" :src="this.$props.src_large" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.vue-enlargeable-image > img {
+  max-width:100%;
+  max-height:100%;
+  cursor:zoom-in;
+}
+.vue-enlargeable-image .vue-enlargeable-image-full {
+  cursor:zoom-out;
+  background-color:transparent;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  background-position: center center;
+  background-repeat:no-repeat;
+  background-size:contain;
+  transition: all 1.4s;
+}
+.vue-enlargeable-image .vue-enlargeable-image-full > img {
+  object-fit:contain;
+  width:100%;
+  height:100%;
+}
+.vue-enlargeable-image .vue-enlargeable-image-full.enlarging {
+  position:fixed;
+  left:0px;
+  top:0px;
+  width:100%;
+  height:100%;
+  background-color:transparent;
+}
+</style>
